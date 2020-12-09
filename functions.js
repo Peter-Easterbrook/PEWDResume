4fd1798b253584471fad475eaa1b0e48b818657b
Peter-Easterbrook <peter.easterbrook@gmail.com>
2020-12-09 23:08:27 +0100

    9.12.2020
    Attempt to add dark mode

--- functions.js
+++ functions.js
@@ -1,26 +1,26 @@
-const SITE = document.querySelector(".site");
-const TRIGGER = document.querySelector(".trigger");
-const REVEAL = document.querySelector(".main-nav");
-const MENUITEMS = REVEAL.querySelectorAll("nav a");
+const SITE = document.querySelector('.site');
+const TRIGGER = document.querySelector('.trigger');
+const REVEAL = document.querySelector('.main-nav');
+const MENUITEMS = REVEAL.querySelectorAll('nav a');
 const MENUARRAY = Array.apply(null, MENUITEMS);
-let screenReaderText = document.querySelector(".trigger .screen-reader-text");
+let screenReaderText = document.querySelector('.trigger .screen-reader-text');
 
 // Toggle reveal class on body element, set aria-expanded and screen reader text on TRIGGER:
 function revealMenu() {
-  SITE.classList.toggle("reveal");
-  REVEAL.classList.add("open");
-  TRIGGER.getAttribute("aria-expanded") == "false"
-    ? TRIGGER.setAttribute("aria-expanded", true)
-    : TRIGGER.setAttribute("aria-expanded", false);
-  screenReaderText.innerHTML == "Reveal menu"
-    ? (screenReaderText.innerHTML = "Hide menu")
-    : (screenReaderText.innerHTML = "Reveal menu");
+  SITE.classList.toggle('reveal');
+  REVEAL.classList.add('open');
+  TRIGGER.getAttribute('aria-expanded') == 'false'
+    ? TRIGGER.setAttribute('aria-expanded', true)
+    : TRIGGER.setAttribute('aria-expanded', false);
+  screenReaderText.innerHTML == 'Reveal menu'
+    ? (screenReaderText.innerHTML = 'Hide menu')
+    : (screenReaderText.innerHTML = 'Reveal menu');
 }
 
 // Hide nav area when focus shifts away:
 function catchFocus() {
   if (
-    TRIGGER.getAttribute("aria-expanded") == "true" &&
+    TRIGGER.getAttribute('aria-expanded') == 'true' &&
     !(
       MENUARRAY.includes(document.activeElement) ||
       document.activeElement === TRIGGER
@@ -33,14 +33,14 @@ function catchFocus() {
 }
 
 function removeMenu() {
-  if (TRIGGER.getAttribute("aria-expanded") === "false") {
-    REVEAL.classList.remove("open");
+  if (TRIGGER.getAttribute('aria-expanded') === 'false') {
+    REVEAL.classList.remove('open');
   }
 }
 // Hide nav area when touch or click happens elsewhere:
 function clickTarget(e) {
   if (
-    TRIGGER.getAttribute("aria-expanded") == "true" &&
+    TRIGGER.getAttribute('aria-expanded') == 'true' &&
     !REVEAL.contains(e.target)
   ) {
     revealMenu();
@@ -48,14 +48,14 @@ function clickTarget(e) {
 }
 
 // Listen for clicks on TRIGGER button:
-TRIGGER.addEventListener("click", revealMenu, false);
+TRIGGER.addEventListener('click', revealMenu, false);
 
 // Listen for focus changes:
-SITE.addEventListener("focusin", catchFocus, true);
+SITE.addEventListener('focusin', catchFocus, true);
 
 // Listen for clicks:
 SITE.addEventListener(
-  "click",
+  'click',
   function (e) {
     clickTarget(e);
   },

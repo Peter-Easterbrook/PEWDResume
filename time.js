072fc73c99f4852612e1c1f88b568cce2d87b490
Peter-Easterbrook <peter.easterbrook@beka-software.at>
2023-05-02 09:24:06 +0200

    Fix am pm on clock

--- time.js
+++ time.js
@@ -2,27 +2,20 @@
   var currentTime = document.getElementById('time');
   setInterval(function () {
     var d = new Date();
-
     var hours = d.getHours(),
       minutes = d.getMinutes(),
-      ampm = 'pm';
-
+      ampm = hours >= 12 && hours < 24 ? 'pm' : 'am';
     if (hours > 12) {
       hours -= 12;
-      ampm = 'pm';
     } else if (hours === 0) {
       hours = 12;
     }
-
     if (minutes < 10) {
       minutes = '0' + minutes;
     }
-
     var sepClass = '';
     if (d.getMinutes() % 2 === 1) sepClass = 'trans';
-
     var sep = '<span class="' + sepClass + '">:</span>';
-
     currentTime.innerHTML = hours + sep + minutes + ' ' + ampm;
   }, 1000);
 })();
